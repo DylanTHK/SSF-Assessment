4032a4af5784087a49b615b5f950d3df61d72b6c
DylanTHK <dylantanhk@gmail.com>
2023-01-27 10:46:47 +0800

    updated structure for html

--- src/main/java/com/ssfAssessment/pizzaApp/controller/PizzaController.java
+++ src/main/java/com/ssfAssessment/pizzaApp/controller/PizzaController.java
@@ -2,23 +2,21 @@ package com.ssfAssessment.pizzaApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PizzaController {
     
-    @GetMapping(path="/")
-    public String getIndex() {
-        return "index";
-    }
 
-    @GetMapping(path="/pizza")
-    public String orderForm() {
-            // @RequestParam String pizza,
-            // @RequestParam String size,
-            // @RequestParam String quantity,
-        // System.out.println("Pizza: " + pizza);
-        // System.out.println("Size: " + size);
-        // System.out.println("Quantity: " + quantity);
+    @PostMapping(path="/pizza")
+    public String orderForm(@RequestParam String pizza,
+    @RequestParam String size,
+    @RequestParam String quantity) {
+            
+        System.out.println("Pizza: " + pizza);
+        System.out.println("Size: " + size);
+        System.out.println("Quantity: " + quantity);
         return "deliveryDetails";
     }
 }
