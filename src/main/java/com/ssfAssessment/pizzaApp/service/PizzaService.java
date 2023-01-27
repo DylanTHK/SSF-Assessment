d009058561804bf4b3e8ba4cbc4f47de5c281f8a
DylanTHK <dylantanhk@gmail.com>
2023-01-27 12:30:46 +0800

    added redisconfig

--- src/main/java/com/ssfAssessment/pizzaApp/service/PizzaService.java
+++ src/main/java/com/ssfAssessment/pizzaApp/service/PizzaService.java
@@ -1,11 +1,22 @@
 package com.ssfAssessment.pizzaApp.service;
 
+import java.util.Random;
+
 public class PizzaService {
     
     // generate Order Id
+    private synchronized String generateId(int numChars) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < numChars) {
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, numChars);
+    }
 
     // calculate order cost
+    
 
     // save order to redis
-    
+
 }
